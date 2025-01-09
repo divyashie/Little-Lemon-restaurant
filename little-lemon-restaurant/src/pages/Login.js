aea603d10253dd3538fe0be56a921155228a48a0
Divya Soopal <d.soopal@gmail.com>
2025-01-09 19:23:56 +0400

    got toggle to work again

---
+++ little-lemon-restaurant/src/pages/Login.js
@@ -0,0 +1,12 @@
+import React from 'react'; 
+
+function Login(){
+    return(
+        <div>
+            <h1>Welcome to Little Lemon!</h1>
+            <p>This is the Login page.</p>
+        </div>
+    );
+}
+
+export default Login; 
